ef0a3165fa899fd2401a4be9d56a4dab9990b7ea
Maximin Joshua <maximinusjoshus@gmail.com>
2022-10-18 23:23:45 +0530

    download command uncommented

--- data_preparation/dataset/download_kaggle_dataset.py
+++ data_preparation/dataset/download_kaggle_dataset.py
@@ -4,7 +4,7 @@ import zipfile
 
 
 os.chdir(os.path.dirname(__file__))
-# subprocess.run(['kaggle', 'competitions', 'download', 'hubmap-organ-segmentation'])
+subprocess.run(['kaggle', 'competitions', 'download', 'hubmap-organ-segmentation'])
 
 with zipfile.ZipFile('hubmap-organ-segmentation.zip', 'r') as zip_ref:
     zip_ref.extractall('hubmap-organ-segmentation/')
